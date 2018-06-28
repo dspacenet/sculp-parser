44e0d548ea0ddca96cbb441c400faf1230ed6e0d
KinIcy NET <nkinicy@gmail.com>
2018-06-28 14:36:28 +0200

    rebuild tests, added test for notify command

--- sculp.test.js
+++ sculp.test.js
@@ -1,13 +1,19 @@
-/* global test expect */
+/* global describe it expect */
 const { SculpParser } = require('./index');
 
-test('"next skip" translates to "skip next skip"', () => {
-  const parser = new SculpParser('next skip');
-  expect(parser.toString()).toBe('skip next skip');
-});
+describe('The SCULP Parser', () => {
+  it('should properly translate "next process"', () => {
+    const parser = new SculpParser('next skip');
+    expect(parser.toString()).toBe('skip next skip');
+  });
 
-test('translating "post(*) throws a SyntaxError', () => {
-  expect(() => new SculpParser('post(*)'))
-    .toThrow(new SyntaxError('Expecting String but found Pattern.'));
-});
+  it('should properly translate "notify(message)"', () => {
+    const parser = new SculpParser('notify("New Message!")');
+    expect(parser.toString()).toBe('enter @ "inbox" do post("New Message!")');
+  });
 
+  it('should throw syntax error when the type of the parameter is not the expected', () => {
+    expect(() => new SculpParser('post(*)'))
+      .toThrow(new SyntaxError('Expecting String but found Pattern.'));
+  });
+});
