c0f6aa91a4d94b475c21bf1aece98afbb051a270
KinIcy NET <nkinicy@gmail.com>
2018-08-04 21:46:39 +0200

    update tests

--- sculp.test.js
+++ sculp.test.js
@@ -3,7 +3,7 @@ const { SculpParser, Expressions } = require('./index');
 
 function patchPosts(procedure) {
   if (procedure.name === 'post') {
-    procedure.params[0].value = `patched ${procedure.params[0].value}`;
+    procedure.params.list[0].value = `patched ${procedure.params.list[0].value}`;
   }
 }
 
@@ -14,90 +14,89 @@ function translatePaths(expression) {
   return expression;
 }
 
+const validSignatures = {
+  notify: [Expressions.String],
+  signal: [Expressions.String],
+  post: [Expressions.String],
+  rm: [Expressions.Pattern, Expressions.Pattern, Expressions.Pattern],
+};
+
 describe('The SCULP Parser', () => {
+  const parser = new SculpParser(validSignatures);
+
   it('should properly parse parallel expressions ', () => {
-    const parser = new SculpParser('skip || skip || skip');
-    expect(parser.toString()).toBe('(skip || skip || skip)');
+    const result = parser.parse('skip || skip || skip');
+    expect(result.toString()).toBe('(skip || skip || skip)');
   });
 
   it('should properly parse "next process"', () => {
-    const parser = new SculpParser('next skip');
-    expect(parser.toString()).toBe('skip next skip');
-  });
-
-  it('should properly parse "enter @ space do process"', () => {
-    const parser = new SculpParser('enter @ "clock" do signal("tick")');
-    expect(parser.toString()).toBe('enter @ "clock" do signal("tick")');
-  });
-
-  it('should properly parse "exit @ space do process"', () => {
-    const parser = new SculpParser('exit @ "clock" do skip');
-    expect(parser.toString()).toBe('exit @ "clock" do skip');
+    const result = parser.parse('next skip');
+    expect(result.toString()).toBe('skip next skip');
   });
 
   it('should properly parse "notify(message)"', () => {
-    const parser = new SculpParser('notify("New Message!")');
-    expect(parser.toString()).toBe('notify("New Message!")');
+    const result = parser.parse('notify("New Message!")');
+    expect(result.toString()).toBe('notify("New Message!")');
+  });
+
+  it('should properly parse "enter @ space do process"', () => {
+    const result = parser.parse('enter @ "clock" do signal("tick")');
+    expect(result.toString()).toBe('enter @ "clock" do signal("tick")');
+  });
+
+  it('should properly parse "exit @ space do process"', () => {
+    const result = parser.parse('exit @ "clock" do skip');
+    expect(result.toString()).toBe('exit @ "clock" do skip');
   });
 
   it('should properly parse "do process until constraint"', () => {
-    const parser = new SculpParser('do post("Bla Bla Bla") until *."stop!".*');
-    expect(parser.toString()).toBe('do post("Bla Bla Bla") until * . "stop!" . *');
+    const result = parser.parse('do post("Bla Bla Bla") until *."stop!".*');
+    expect(result.toString()).toBe('do post("Bla Bla Bla") until * . "stop!" . *');
   });
 
   it('should properly parse message constraints', () => {
-    const parser = new SculpParser('when msg:"frank":*."?" do post("Hi Frank! I will answer your question asap.")');
-    expect(parser.toString()).toBe('when msg:"frank":* . "?" do post("Hi Frank! I will answer your question asap.")');
-  });
-
-  it('should properly parse message-content constraint', () => {
-    const parser = new SculpParser('when msg-content:"Hello" do post("Hi!")');
-    expect(parser.toString()).toBe('when msg-content:"Hello" do post("Hi!")');
-  });
-
-  it('should properly parse message-user constraint', () => {
-    const parser = new SculpParser('when msg-user:"frank" do rm(*,"frank",*)');
-    expect(parser.toString()).toBe('when msg-user:"frank" do rm(*, "frank", *)');
+    const result = parser.parse('when user:"frank" ^ msg:*."?" do post("Hi Frank! I will answer your question asap.")');
+    expect(result.toString()).toBe('when (user: "frank" ^ msg: * . "?") do post("Hi Frank! I will answer your question asap.")');
   });
 
   it('should replace a placeholder with its corresponding insert', () => {
-    const parser = new SculpParser('post($message)', { message: new Expressions.String('Hello World!') });
-    expect(parser.toString()).toBe('post("Hello World!")');
+    const result = parser.parse('post($message)', { message: new Expressions.String('Hello World!') });
+    expect(result.toString()).toBe('post("Hello World!")');
   });
 
-  it('should throw syntax error when the type of the parameter is not the expected', () => {
-    expect(() => new SculpParser('post(*)'))
-      .toThrow(new SyntaxError('Expecting String but found Pattern.'));
+  it('should throw type error when the type of the parameter is not the expected', () => {
+    expect(() => parser.parse('post(*)'))
+      .toThrow(new TypeError('Parameter at position 0 of post must be of type String instead of Pattern.'));
   });
 
   it('should throw reference error when a placeholder is set without its corresponding insert', () => {
-    expect(() => new SculpParser('post($message)', { text: new Expressions.String('Hi!') }))
+    expect(() => parser.parse('post($message)', { text: new Expressions.String('Hi!') }))
       .toThrow(new ReferenceError('Insert for placeholder \'message\' not found.'));
   });
 
   describe('when the translation is done', () => {
     it('should be able to traverse simple expressions', () => {
-      const parser = new SculpParser('post("message")');
-      parser.applyTo(Expressions.Procedure, patchPosts);
-      expect(parser.toString()).toBe('post("patched message")');
+      const result = parser.parse('post("message")');
+      result.applyTo(Expressions.Procedure, patchPosts);
+      expect(result.toString()).toBe('post("patched message")');
     });
 
     it('should be able to traverse complex expressions', () => {
-      const parser = new SculpParser('post("message") || signal("un-patched") || post("another message")');
-      parser.applyTo(Expressions.Procedure, patchPosts);
-      expect(parser.toString()).toBe('(post("patched message") || signal("un-patched") || post("patched another message"))');
+      const result = parser.parse('post("message") || signal("un-patched") || post("another message")');
+      result.applyTo(Expressions.Procedure, patchPosts);
+      expect(result.toString()).toBe('(post("patched message") || signal("un-patched") || post("patched another message"))');
     });
 
     it('should be able to patch simple expressions', () => {
-      const parser = new SculpParser('enter @ "clock" do post("tick")');
-      parser.patch(translatePaths);
-      expect(parser.toString()).toBe('enter 6 do post("tick")');
+      const result = parser.parse('enter @ "clock" do post("tick")');
+      result.patch(translatePaths);
+      expect(result.toString()).toBe('enter 6 do post("tick")');
     });
 
     it('should be able to patch complex expressions', () => {
-      const parser = new SculpParser('enter @ "clock" do when "tick" do exit @ "clock" do post("tack")');
-      parser.patch(translatePaths);
-      expect(parser.toString()).toBe('enter 6 do when "tick" do exit 6 do post("tack")');
+      const result = parser.parse('enter @ "clock" do when "tick" do exit @ "clock" do post("tack")');
+      result.patch(translatePaths);
+      expect(result.toString()).toBe('enter 6 do when "tick" do exit 6 do post("tack")');
     });
   });
 });
